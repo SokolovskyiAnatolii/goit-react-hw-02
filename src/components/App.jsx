694c51d13ab54adbab10e58cf2b0c80937836cfb
AlexandrD\AlexandrD <sans357@gmail.com>
2025-01-14 09:49:01 +0200

    hw2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,82 @@
-import Profile from './profile/Profile';
-import userData from './profile/userData.json';
+import { Component } from 'react';
+import Description from './Description/Description';
+import Feedback from './Feedback/Feedback';
+import Options from './Options/Options';
+import Notification from './Notification/Notification';
 
-import FriendList from './friends/FriendList/friendsList';
-import friends from './friends/friends.json';
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-import TransactionHistory from './transactions/transactionHistory';
-import transactions from './transactions/transactions.json';
+  componentDidMount() {
+    const savedFeedback = JSON.parse(localStorage.getItem('feedback'));
+    if (savedFeedback) {
+      this.setState(savedFeedback);
+    }
+  }
 
-function App() {
-  return (
-    <>
-      <Profile
-        username={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        avatar={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
-  );
+  componentDidUpdate(_, prevState) {
+    if (prevState !== this.state) {
+      localStorage.setItem('feedback', JSON.stringify(this.state));
+    }
+  }
+
+  updateFeedback = feedbackType => {
+    this.setState(prevState => ({
+      [feedbackType]: prevState[feedbackType] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, value) => acc + value, 0);
+  };
+
+  countPositiveFeedback = (totalFeedback, good) => {
+    return Math.round((good / totalFeedback) * 100);
+  };
+
+  handleReset = () => {
+    this.setState(prevState =>
+      Object.keys(prevState).reduce((acc, key) => {
+        acc[key] = 0;
+        return acc;
+      }, {})
+    );
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const options = Object.keys(this.state);
+    const totalFeedback = this.countTotalFeedback();
+    const positiveFeedback = totalFeedback ? this.countPositiveFeedback(totalFeedback, good) : 0;
+
+    return (
+      <>
+        <Description />
+        <Options
+          options={options}
+          onClick={this.updateFeedback}
+          onReset={this.handleReset}
+          totalFeedback={totalFeedback}
+        />
+
+        {totalFeedback > 0 ? (
+          <Feedback
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            totalFeedback={totalFeedback}
+            positiveFeedback={positiveFeedback}
+          />
+        ) : (
+          <Notification message="No feedback yet" />
+        )}
+      </>
+    );
+  }
 }
+
 export default App;
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import { text } from './Notification.module.css';
+
+const Notification = ({ message }) => {
+  return <p className={text}>{message}</p>;
+};
+
+Notification.propTypes = {
+  message: PropTypes.string,
+};
+
+Notification.propTypes = {
+  message: PropTypes.string,
+};
+
+export default Notification;
